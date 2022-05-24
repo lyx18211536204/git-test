1cb59374297c69c4dc7801c30eee87e7ecc6afdd
LinYueXin <LinYueXin@qq.com>
2022-05-24 16:26:53 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!second commit!");
         System.out.println("hello git!third commit!");
+        System.out.println("hello git!hot-fix commit1!");
     }
 }
