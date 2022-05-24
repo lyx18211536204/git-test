a6060cdcdb427ef26d0b151be5976d85a680e90b
LinYueXin <LinYueXin@qq.com>
2022-05-24 16:09:06 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!second commit!");
+        System.out.println("hello git!third commit!");
     }
 }
